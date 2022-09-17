89e002b16290e08df122ebffb3972bf9b130a575
Victor Omboke <victoromboke@gmail.com>
2022-09-16 17:28:12 -0700

    Add variables for a new function along with console.log functions
    I have created five new variables for a new function that simulates one round of a game of rock paper scissors. The variable 'playerSelection' takes the users input of 'rock', 'paper', or 'scissors'. The variable 'computerSelection' prints the outcome of the previous fucntion 'getComputerChoice'. The variables 'rock', 'paper', and 'scissors' simply print out their name in a string. The function 'playRound' takes two parameters, 'playerSelection' and 'computerSelection' and plays one round of rock paper scissors by comparing the users input with the computers randomly generated selection.
    The console.log functions serve as a method of showing the user what the function is out putting and also showing the users choice, the computers choice, and the overall result of who won.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -12,8 +12,31 @@ function getComputerChoice() {
         return "Scissors";
     }
 }
-console.log(getComputerChoice());
 
-function rockPaperScissors(playerSelection, computerSelection) {
+const playerSelection = prompt("Please type 'Rock', 'Paper', or 'Scissors'!", "Rock");
+const computerSelection = getComputerChoice();
+const rock = "Rock";
+const paper = "Paper";
+const scissors = "Scissors";
 
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+        return "Looks like a tie, try again!";
+    } else if (playerSelection.toLowerCase() == rock.toLowerCase() && computerSelection.toLowerCase() == paper.toLowerCase()) {
+        return "You lose, paper beats rock!";
+    } else if (playerSelection.toLowerCase() == rock.toLowerCase() && computerSelection.toLowerCase() == scissors.toLowerCase()) {
+        return "You win, rock beats Scissors!";
+    } else if (playerSelection.toLowerCase() == paper.toLowerCase() && computerSelection.toLowerCase() == rock.toLowerCase()) {
+        return "You win, paper beats rock!";
+    } else if (playerSelection.toLowerCase() == paper.toLowerCase() && computerSelection.toLowerCase() == scissors.toLowerCase()) {
+        return "You lose, Scissors beats paper!";
+    } else if (playerSelection.toLowerCase() == scissors.toLowerCase() && computerSelection.toLowerCase() == rock.toLowerCase()) {
+        return "You lose, Rock beats scissors!";
+    } else if (playerSelection.toLowerCase() == scissors.toLowerCase() && computerSelection.toLowerCase() == paper.toLowerCase()) {
+        return "You win, scissors beats paper!";
+    }
 }
+
+console.log(`Your choice: ${playerSelection}`);
+console.log(`Computers choice: ${computerSelection}`);
+console.log(`Result: ${playRound(playerSelection, computerSelection)}`);
