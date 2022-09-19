7f436067e690089e2cd8fc721a4b9e55a72c3dbd
Victor Omboke <victoromboke@gmail.com>
2022-09-19 13:37:25 -0700

    Remove variables, clean up 'playRound' function, and implement 'game' function
    First change made was to remove 'rock', 'paper', 'scissors' variables as they were not necessary which lead to cleaning upthe 'playRound' function and making the code a bit easier to read.
    The next change was creating a new function called 'game' which loops 'playRound' five times to play five rounds of rock, paper, scissors.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,4 +1,4 @@
-
+//Randomize computers choice
 function random() {
     return Math.floor(Math.random() * 3) + 1;
 }
@@ -15,36 +15,38 @@ function getComputerChoice() {
 
 const playerSelection = prompt("Please type 'Rock', 'Paper', or 'Scissors'!", "Rock");
 const computerSelection = getComputerChoice();
-const rock = "rock";
-const paper = "paper";
-const scissors = "scissors";
+
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
         return "Looks like a tie, try again!";
-    } else if (playerSelection.toLowerCase() == rock && computerSelection == paper) {
+    } else if (playerSelection.toLowerCase() == "rock" && computerSelection == "paper") {
         return "You lose, paper beats rock!";
-    } else if (playerSelection.toLowerCase() == rock && computerSelection == scissors) {
+    } else if (playerSelection.toLowerCase() == "rock" && computerSelection == "scissors") {
         return "You win, rock beats Scissors!";
-    } else if (playerSelection.toLowerCase() == paper && computerSelection == rock) {
+    } else if (playerSelection.toLowerCase() == "paper" && computerSelection == "rock") {
         return "You win, paper beats rock!";
-    } else if (playerSelection.toLowerCase() == paper && computerSelection == scissors) {
+    } else if (playerSelection.toLowerCase() == "paper" && computerSelection == "scissors") {
         return "You lose, Scissors beats paper!";
-    } else if (playerSelection.toLowerCase() == scissors && computerSelection == rock) {
+    } else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "rock") {
         return "You lose, Rock beats scissors!";
-    } else if (playerSelection.toLowerCase() == scissors && computerSelection == paper) {
+    } else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "paper") {
         return "You win, scissors beats paper!";
     }
 }
 
-// function game() {
-//     for (let i = 0; i < 5; i++) {
-//         playRound(playerSelection, computerSelection);
-//     }
-//     console.log(`Your choice: ${playerSelection.toLowerCase()}`);
-//     console.log(`Computers choice: ${computerSelection}`);
-//     console.log(`Result: ${playRound(playerSelection.toLowerCase(), computerSelection)}`);
-// }
 
-// game();
+function game() {
+    for (let i = 1; i < 6; i++) {
+        const playerSelection = prompt("Please type 'Rock', 'Paper', or 'Scissors'!", "Rock");
+        const computerSelection = getComputerChoice();
+        playRound(playerSelection, computerSelection);
+        console.log(`Round: ${i}`)
+        console.log(`Your choice: ${playerSelection.toLowerCase()}`);
+        console.log(`Computers choice: ${computerSelection}`);
+        console.log(`Result: ${playRound(playerSelection.toLowerCase(), computerSelection)}`);
+    }
+}
+
+game();
 
