f7f45b34d5c87741ffe45db1f1c05aba2cfe5b08
Victor Omboke <victoromboke@gmail.com>
2022-10-16 16:17:47 -0700

    Remove unnecessary code

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -57,19 +57,6 @@ function playRound(playerSelection, computerSelection) {
 
 // game();
 
-const container = document.querySelector('#container');
 
-const rock = document.createElement('button');
-rock.classList.add('rock');
-rock.textContent = 'Rock';
-container.appendChild(rock);
 
-const paper = document.createElement('button');
-paper.classList.add('paper');
-paper.textContent = 'Paper';
-container.appendChild(paper);
 
-const scissors = document.createElement('button');
-scissors.classList.add('scissors');
-scissors.textContent = 'Scissors';
-container.appendChild(scissors);
